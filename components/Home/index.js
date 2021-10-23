7991bb3c15efe100aeeb2e765f8887040b783c4c
suntiver_tid <suntiver6@gmail.com>
2021-10-23 19:37:09 +0700

    add insection

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,11 +1,18 @@
-import React from 'react'
-import GlobalStyle from '../globals.styled';
-import Navbar from '../Navbar';
+import React from "react";
+import GlobalStyle from "../globals.styled";
+import Navbar from "../Navbar";
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data';
+import InfoSection  from '../InfoSection';
+
 export default function Home() {
-    return (
-        <React.Fragment>
-            <GlobalStyle/>
-            <Navbar/>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <Navbar />
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjThree} />
+      <InfoSection {...homeObjTwo} />
+      <InfoSection {...homeObjFour} />
+    </React.Fragment>
+  );
 }
